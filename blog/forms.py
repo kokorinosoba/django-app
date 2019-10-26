0a3bca37237141ca2625bc2f1cdeb374006e2cd9
kokorinosoba <34607448+kokorinosoba@users.noreply.github.com>
2019-10-27 06:31:08 +0900

    チェックボックスのサンプルを実装

--- blog/forms.py
+++ blog/forms.py
@@ -7,3 +7,53 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ('title', 'text', )
+
+
+class ChkForm(forms.Form):
+    labels = ['チェック', '複数チェック', 'ラジオボタン', '動的選択肢１', '動的選択肢２']
+    SINGLE_CHOICE = [('1', 'OKなららチェック')]
+    CHOICE = [
+      ('1', '選択肢＜１＞'),
+      ('2', '選択肢＜２＞'),
+      ('3', '選択肢＜３＞')]
+
+    one = forms.MultipleChoiceField(
+        label=labels[0],
+        required=False,
+        disabled=False,
+        initial=[],
+        choices=SINGLE_CHOICE,
+        widget=forms.CheckboxSelectMultiple(attrs={
+          'id': 'one', 'class': 'form-check-input'}))
+
+    two = forms.MultipleChoiceField(
+        label=labels[1],
+        required=False,
+        disabled=False,
+        initial=[],
+        choices=CHOICE,
+        widget=forms.CheckboxSelectMultiple(attrs={
+          'id': 'two', 'class': 'form-check-input'}))
+
+    four = forms.MultipleChoiceField(
+        label=labels[3],
+        required=False,
+        disabled=False,
+        widget=forms.CheckboxSelectMultiple(attrs={
+          'id': 'four', 'class': 'form-check-input'}))
+
+    three = forms.MultipleChoiceField(
+        label=labels[2],
+        required=False,
+        disabled=False,
+        initial=['2'],
+        choices=CHOICE,
+        widget=forms.RadioSelect(attrs={
+          'id': 'three', 'class': 'form-check-input'}))
+
+    five = forms.MultipleChoiceField(
+        label=labels[4],
+        required=False,
+        disabled=False,
+        widget=forms.RadioSelect(attrs={
+          'id': 'five', 'class': 'form-check-input'}))
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
+from django.template.context_processors import csrf
 from .models import Post
-from .forms import PostForm
+from .forms import PostForm, ChkForm
 
 
 # Create your views here.
@@ -42,3 +43,35 @@ def post_edit(request, pk):
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
+
+
+def demo3(request):
+    labels = ['チェック', '複数チェック', 'ラジオボタン', '動的選択肢１', '動的選択肢２']
+    # 入力結果を格納する辞書
+    results = {}
+    radios = {}
+    ret = ''
+    if request.method == 'POST':
+        # 入力されたデータの受取
+        results[labels[0]] = request.POST.getlist("one")
+        results[labels[1]] = request.POST.getlist("two")
+        results[labels[2]] = request.POST.getlist("three")
+        results[labels[3]] = request.POST.getlist("four")
+        results[labels[4]] = request.POST.getlist("five")
+        ret = 'OK'
+        c = {'results': results, 'ret': ret}
+    else:
+        form = ChkForm()
+        choice1 = []
+        choice1.append(('1', '動的選択肢１'))
+        choice1.append(('2', '動的選択肢２'))
+        choice1.append(('3', '動的選択肢３'))
+        choice1.append(('4', '動的選択肢４'))
+        form.fields['four'].choices = choice1
+        form.fields['four'].initial = ['2']
+        form.fields['five'].choices = choice1
+        form.fields['five'].initial = ['3']
+        c = {'form': form, 'ret': ret}
+        # CFRF対策（必須）
+        c.update(csrf(request))
+    return render(request, 'demo03.html', c)
